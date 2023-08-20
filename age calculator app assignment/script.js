a588998f9e7362dd4db01ab8814c85b969554cf3
divinedibobie <57143668+divinedibobie@users.noreply.github.com>
2023-08-20 12:33:30 -0700

    Add files via upload

---
+++ age calculator app assignment/script.js
@@ -0,0 +1,14 @@
+alert("get todays date");
+let btn=document.queryselector ("#btn")
+let btn2=document.queryselector ("#btn2")
+let p=document.queryselector ('#paragraph')
+
+btn.addEventListener ["click",() =>{
+    localStorage.setItem('foundation','HerTechTrail');
+}]
+let button ="hertechTrailers";
+
+btn.addEventListener ["click",() =>{
+    localStorage.setItem('foundation','HerTechTrail');
+}]
+let button ="hertechTrailers";                                          
